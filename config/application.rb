a2566de34953092d7c421ac1c150ad6a74d89f57
cuerci <lucas.cuerci25@gmail.com>
2024-12-19 10:28:35 -0300

    traducao do devise e ajuste nas views

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module MeuAmigoOculto
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = 'Brasilia'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :"pt-BR"
   end
 end
